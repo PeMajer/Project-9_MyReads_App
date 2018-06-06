590af0298dfd93d7a1072e4fd61b5af0152b3eed
Petr Majer <majer.petr@gmail.com>
2018-06-06 09:09:10 +0200

    Feat: Get books from API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import * as BooksAPI from './BooksAPI';
 
 class BooksApp extends React.Component {
   state = {
@@ -10,7 +11,14 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: false,
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then( (allBooks) => {      
+      this.setState({books: allBooks})
+    })   
   }
 
   render() {
