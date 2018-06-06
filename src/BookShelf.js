aaa0b29f2c5c43aef4b195df9725f9f65a99a48b
Petr Majer <majer.petr@gmail.com>
2018-06-06 20:02:49 +0200

    Refactor: Create one wiev component BookShelf
    refactor code from two component into one

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 
-class CurentlyReading extends Component {
+class BookShelf extends Component {
   render() {
-    const { readingBooks } = this.props
+    const { books, title } = this.props
 
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
+        <h2 className="bookshelf-title">{title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            {readingBooks.map( (book) =>
+            {books.map( (book) =>
               <li key={book.id}>
                 <div className="book">
                   <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
                     <div className="book-shelf-changer">
-                      <select>
+                      <select defaultValue={book.shelf}>
                         <option value="none" disabled>Move to...</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
@@ -36,4 +36,4 @@ class CurentlyReading extends Component {
   }
 }
 
-export default CurentlyReading;
+export default BookShelf;
