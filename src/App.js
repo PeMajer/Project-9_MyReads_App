f09a2f475f3479a0b56bb1d1637b99b8e7744bc8
Petr Majer <majer.petr@gmail.com>
2018-06-08 08:59:03 +0200

    Feat: Add route to App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
 import * as BooksAPI from './BooksAPI';
-import BookShelf from './BookShelf'
+import { Route } from 'react-router-dom';
+import BookShelf from './BookShelf';
 import SearchBooks from './SearchBooks';
 
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     books: []
   }
 
@@ -39,12 +33,15 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
+
+        <Route path='/search' render={ () => (
           <SearchBooks
             books={this.state.books}
             onUpdateBook={this.updateBook}
           />
-        ) : (
+        )} />
+
+        <Route exact path='/' render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -52,27 +49,28 @@ class BooksApp extends React.Component {
             <div className="list-books-content">
               <div>
                 <BookShelf
-                  books={this.state.books.filter((book)=> book.shelf === 'currentlyReading')}
+                  books={this.state.books.filter((book) => book.shelf === 'currentlyReading')}
                   title='Currently Reading'
                   onUpdateBook={this.updateBook}
-                 />
+                />
                 <BookShelf
-                  books={this.state.books.filter((book)=> book.shelf === 'wantToRead')}
+                  books={this.state.books.filter((book) => book.shelf === 'wantToRead')}
                   title='Want to read'
                   onUpdateBook={this.updateBook}
-                 />
+                />
                 <BookShelf
-                  books={this.state.books.filter((book)=> book.shelf === 'read')}
+                  books={this.state.books.filter((book) => book.shelf === 'read')}
                   title='Read'
                   onUpdateBook={this.updateBook}
-                 />
+                />
               </div>
             </div>
             <div className="open-search">
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
             </div>
           </div>
-        )}
+        )} />
+
       </div>
     )
   }
